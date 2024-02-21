4b3e3f07fd5e2de9b1f31f624768e7f1addae36f
Manjunath <manjunath.anawal3@gmail.com>
2024-02-21 20:50:27 +0530

    Initial Commit

---
+++ FavoriteDog/FavoriteDogApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavoriteDogApp.swift
+//  FavoriteDog
+//
+//  Created by Manjunath Anawal on 21/02/24.
+//
+
+import SwiftUI
+
+@main
+struct FavoriteDogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
